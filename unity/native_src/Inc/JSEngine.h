b1f8aad966991ba61ae59d6b11aeba8f09fad0d4
zombieyang <zombieyang@tencent.com>
2021-07-19 13:04:40 +0800

    one arguments

--- unity/native_src/Inc/JSEngine.h
+++ unity/native_src/Inc/JSEngine.h
@@ -76,7 +76,7 @@ v8::Local<v8::ArrayBuffer> NewArrayBuffer(v8::Isolate* Isolate, void *Ptr, size_
 class JSEngine
 {
 public:
-    JSEngine(void* external_quickjs_runtime, void* external_quickjs_context);
+    JSEngine(void* external_quickjs_context);
 
     ~JSEngine();
 
