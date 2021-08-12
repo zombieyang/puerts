18ad8c0db3ec9883ca921b30ae77274024d3075f
zombieyang <zombieyang@tencent.com>
2021-08-12 18:00:55 +0800

    update patch

--- unity/native_src_ourv8/Src/JSEngine_NativeClasses.cpp
+++ unity/native_src_ourv8/Src/JSEngine_NativeClasses.cpp
@@ -73,22 +73,7 @@ const ClassManager = {                                                          
     },                                                                                  \
                                                                                         \
     makeGlobalFunction(name, handler) {                                                 \
-        (function() {                                                                   \
-            this[name] = function() {                                                   \
-                if (arguments.length == 0)                                              \
-                    return PuertsV8.drCallback.call(handler, this);                     \
-                else if (arguments.length == 1)                                         \
-                    return PuertsV8.drCallback.call(handler, this, arguments[0]);       \
-                else if (arguments.length == 2)                                         \
-                    return PuertsV8.drCallback.call(handler, this, arguments[0], arguments[1]);       \
-                else if (arguments.length == 3)                                         \
-                    return PuertsV8.drCallback.call(handler, this, arguments[0], arguments[1], arguments[2]);       \
-                else if (arguments.length == 4)                                         \
-                    return PuertsV8.drCallback.call(handler, this, arguments[0], arguments[1], arguments[2], arguments[3]);       \
-                else                                                                    \
-                    return PuertsV8.drCallback.call(handler, this, ...arguments);       \
-            };                                                                          \
-        })()                                                                            \
+        global[name] = PuertsV8.makeCallback(name, handler);                            \
     },                                                                                  \
 };                                                                                      \
                                                                                         \
