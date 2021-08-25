72790be7568cf18b44898cb29592e3fd8e12220d
zombieyang <zombieyang@tencent.com>
2021-08-25 15:34:07 +0800

    .

--- nodejs-build/windows_32.cmd
+++ nodejs-build/windows_32.cmd
@@ -19,5 +19,7 @@ copy node/node-%VERSION%/src/node_version.h ./puerts-node/include
 xcopy /E /I node/node-%VERSION%/deps/uv/include ./puerts-node/deps/uv
 xcopy /E /I node/node-%VERSION%/deps/v8/include ./puerts-node/deps/v8
 
-md puerts-node/lib/macOS/
-copy node/node-%VERSION%/out/Release/libnode.83.dylib ./puerts-node/lib/macOS/
+md puerts-node/lib/Win32/
+copy node/node-%VERSION%/out/Release/libnode.dll ./puerts-node/lib/Win32/
+copy node/node-%VERSION%/out/Release/libnode.exp ./puerts-node/lib/Win32/
+copy node/node-%VERSION%/out/Release/libnode.lib ./puerts-node/lib/Win32/
