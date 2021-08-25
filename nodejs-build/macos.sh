9e14fbd0e741040f639fb66c4866ef0cbc31a04f
zombieyang <zombieyang@tencent.com>
2021-08-25 14:57:44 +0800

    path

--- nodejs-build/macos.sh
+++ nodejs-build/macos.sh
@@ -12,14 +12,14 @@ git checkout v$VERSION
 ./configure --shared
 make -j8
 
-mkdir -p puerts-node/include
-mkdir -p puerts-node/deps/uv/include
-mkdir -p puerts-node/deps/v8/include
+mkdir -p ../puerts-node/include
+mkdir -p ../puerts-node/deps/uv/include
+mkdir -p ../puerts-node/deps/v8/include
 
-cp src/node.h ./puerts-node/include
-cp src/node_version.h ./puerts-node/include
-cp -r deps/uv/include ./puerts-node/deps/uv
-cp -r deps/v8/include ./puerts-node/deps/v8
+cp src/node.h ../puerts-node/include
+cp src/node_version.h ../puerts-node/include
+cp -r deps/uv/include ../puerts-node/deps/uv
+cp -r deps/v8/include ../puerts-node/deps/v8
 
-mkdir -p puerts-node/lib/macOS/
-cp out/Release/libnode.83.dylib ./puerts-node/lib/macOS/
+mkdir -p ../puerts-node/lib/macOS/
+cp out/Release/libnode.83.dylib ../puerts-node/lib/macOS/
--- nodejs-build/windows_64.cmd
+++ nodejs-build/windows_64.cmd
@@ -1,5 +1,6 @@
 set VERSION=%1
 
+cd %HOMEPATH%
 powershell -command "Invoke-WebRequest https://github.com/nodejs/node/archive/refs/tags/v%VERSION%.zip -O node.zip"
 7z x node.zip -o*
 
@@ -7,14 +8,16 @@ cd node/node-%VERSION%
 
 .\vcbuild.bat dll openssl-no-asm
 
+cd %HOMEPATH%
+
 md puerts-node/include
 md puerts-node/deps/uv/include
 md puerts-node/deps/v8/include
 
-copy src/node.h ./puerts-node/include
-copy src/node_version.h ./puerts-node/include
-xcopy /E /I deps/uv/include ./puerts-node/deps/uv
-xcopy /E /I deps/v8/include ./puerts-node/deps/v8
+copy node/node-%VERSION%/src/node.h ./puerts-node/include
+copy node/node-%VERSION%/src/node_version.h ./puerts-node/include
+xcopy /E /I node/node-%VERSION%/deps/uv/include ./puerts-node/deps/uv
+xcopy /E /I node/node-%VERSION%/deps/v8/include ./puerts-node/deps/v8
 
 md puerts-node/lib/macOS/
-copy out/Release/libnode.83.dylib ./puerts-node/lib/macOS/
+copy node/node-%VERSION%/out/Release/libnode.83.dylib ./puerts-node/lib/macOS/
