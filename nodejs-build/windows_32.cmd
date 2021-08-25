73658210bf59cf8f8155eabe92125122e58a797d
zombieyang <zombieyang@tencent.com>
2021-08-25 16:19:24 +0800

    use git

--- nodejs-build/windows_32.cmd
+++ nodejs-build/windows_32.cmd
@@ -1,26 +1,26 @@
 set VERSION=%1
 
 cd %HOMEPATH%
-git -h
-@REM powershell -command "Invoke-WebRequest https://github.com/nodejs/node/archive/refs/tags/v%VERSION%.zip -O node.zip"
-@REM 7z x node.zip -o*
+git clone --single-branch -b v14.x --no-tags https://github.com/nodejs/node.git
 
-@REM cd node/node-%VERSION%
+cd node
+git fetch v%VERSION%
+git checkout v%VERSION%
 
-@REM .\vcbuild.bat dll openssl-no-asm
+.\vcbuild.bat dll openssl-no-asm
 
-@REM cd %HOMEPATH%
+cd %HOMEPATH%
 
-@REM md puerts-node/include
-@REM md puerts-node/deps/uv/include
-@REM md puerts-node/deps/v8/include
+md puerts-node/include
+md puerts-node/deps/uv/include
+md puerts-node/deps/v8/include
 
-@REM copy node/node-%VERSION%/src/node.h ./puerts-node/include
-@REM copy node/node-%VERSION%/src/node_version.h ./puerts-node/include
-@REM xcopy /E /I node/node-%VERSION%/deps/uv/include ./puerts-node/deps/uv
-@REM xcopy /E /I node/node-%VERSION%/deps/v8/include ./puerts-node/deps/v8
+copy node/src/node.h ./puerts-node/include
+copy node/src/node_version.h ./puerts-node/include
+xcopy /E /I node/deps/uv/include ./puerts-node/deps/uv
+xcopy /E /I node/deps/v8/include ./puerts-node/deps/v8
 
-@REM md puerts-node/lib/Win32/
-@REM copy node/node-%VERSION%/out/Release/libnode.dll ./puerts-node/lib/Win32/
-@REM copy node/node-%VERSION%/out/Release/node.exp ./puerts-node/lib/Win32/
-@REM copy node/node-%VERSION%/out/Release/node.lib ./puerts-node/lib/Win32/
+md puerts-node/lib/Win32/
+copy node/out/Release/libnode.dll ./puerts-node/lib/Win32/
+copy node/out/Release/node.exp ./puerts-node/lib/Win32/
+copy node/out/Release/node.lib ./puerts-node/lib/Win32/
--- nodejs-build/windows_64.cmd
+++ nodejs-build/windows_64.cmd
@@ -1,10 +1,11 @@
 set VERSION=%1
 
 cd %HOMEPATH%
-powershell -command "Invoke-WebRequest https://github.com/nodejs/node/archive/refs/tags/v%VERSION%.zip -O node.zip"
-7z x node.zip -o*
+git clone --single-branch -b v14.x --no-tags https://github.com/nodejs/node.git
 
-cd node/node-%VERSION%
+cd node
+git fetch v%VERSION%
+git checkout v%VERSION%
 
 .\vcbuild.bat dll openssl-no-asm
 
@@ -14,12 +15,12 @@ md puerts-node/include
 md puerts-node/deps/uv/include
 md puerts-node/deps/v8/include
 
-copy node/node-%VERSION%/src/node.h ./puerts-node/include
-copy node/node-%VERSION%/src/node_version.h ./puerts-node/include
-xcopy /E /I node/node-%VERSION%/deps/uv/include ./puerts-node/deps/uv
-xcopy /E /I node/node-%VERSION%/deps/v8/include ./puerts-node/deps/v8
+copy node/src/node.h ./puerts-node/include
+copy node/src/node_version.h ./puerts-node/include
+xcopy /E /I node/deps/uv/include ./puerts-node/deps/uv
+xcopy /E /I node/deps/v8/include ./puerts-node/deps/v8
 
 md puerts-node/lib/Win64/
-copy node/node-%VERSION%/out/Release/libnode.dll ./puerts-node/lib/Win64/
-copy node/node-%VERSION%/out/Release/node.exp ./puerts-node/lib/Win64/
-copy node/node-%VERSION%/out/Release/node.lib ./puerts-node/lib/Win64/
+copy node/out/Release/libnode.dll ./puerts-node/lib/Win64/
+copy node/out/Release/node.exp ./puerts-node/lib/Win64/
+copy node/out/Release/node.lib ./puerts-node/lib/Win64/
