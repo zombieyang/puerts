04580e56714e5305b34b4421d3a47ced2af1f035
zombieyang(杨浩) <zombieyang@tencent.com>
2021-08-25 17:46:10 +0800

    .

--- nodejs-build/windows_64.cmd
+++ nodejs-build/windows_64.cmd
@@ -11,20 +11,20 @@ node %GITHUB_WORKSPACE%\v8-build\CRLF2LF.js %GITHUB_WORKSPACE%\nodejs-build\node
 call git apply --cached --reject %GITHUB_WORKSPACE%\nodejs-build\nodemod.patch
 call git checkout -- .
 
-.\vcbuild.bat dll openssl-no-asm
+@REM .\vcbuild.bat dll openssl-no-asm
 
-cd %HOMEPATH%
+@REM cd %HOMEPATH%
 
-md puerts-node/include
-md puerts-node/deps/uv/include
-md puerts-node/deps/v8/include
+@REM md puerts-node/include
+@REM md puerts-node/deps/uv/include
+@REM md puerts-node/deps/v8/include
 
-copy node/src/node.h ./puerts-node/include
-copy node/src/node_version.h ./puerts-node/include
-xcopy /E /I node/deps/uv/include ./puerts-node/deps/uv
-xcopy /E /I node/deps/v8/include ./puerts-node/deps/v8
+@REM copy node/src/node.h ./puerts-node/include
+@REM copy node/src/node_version.h ./puerts-node/include
+@REM xcopy /E /I node/deps/uv/include ./puerts-node/deps/uv
+@REM xcopy /E /I node/deps/v8/include ./puerts-node/deps/v8
 
-md puerts-node/lib/Win64/
-copy node/out/Release/libnode.dll ./puerts-node/lib/Win64/
-copy node/out/Release/libnode.exp ./puerts-node/lib/Win64/
-copy node/out/Release/libnode.lib ./puerts-node/lib/Win64/
+@REM md puerts-node/lib/Win64/
+@REM copy node/out/Release/libnode.dll ./puerts-node/lib/Win64/
+@REM copy node/out/Release/libnode.exp ./puerts-node/lib/Win64/
+@REM copy node/out/Release/libnode.lib ./puerts-node/lib/Win64/
