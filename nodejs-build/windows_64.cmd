fa4ec064f555d71b3a865cd6a379eaac98ee00a5
zombieyang <zombieyang@tencent.com>
2021-08-26 17:34:33 +0800

    command syntax

--- nodejs-build/windows_64.cmd
+++ nodejs-build/windows_64.cmd
@@ -1,15 +1,15 @@
 cd %HOMEPATH%
 
-md puerts-node/include
-md puerts-node/deps/uv/include
-md puerts-node/deps/v8/include
+md puerts-node\include
+md puerts-node\deps\uv\include
+md puerts-node\deps\v8\include
 
-copy node/src/node.h ./puerts-node/include
-copy node/src/node_version.h ./puerts-node/include
-xcopy /E /I node/deps/uv/include ./puerts-node/deps/uv
-xcopy /E /I node/deps/v8/include ./puerts-node/deps/v8
+copy node\src\node.h .\puerts-node\include
+copy node\src\node_version.h .\puerts-node\include
+xcopy /E /I node\deps\uv\include .\puerts-node\deps\uv /s/h/e/k/f/c
+xcopy /E /I node\deps\v8\include .\puerts-node\deps\v8 /s/h/e/k/f/c
 
-md puerts-node/lib/Win64/
-copy node/out/Release/libnode.dll ./puerts-node/lib/Win64/
-copy node/out/Release/libnode.exp ./puerts-node/lib/Win64/
-copy node/out/Release/libnode.lib ./puerts-node/lib/Win64/
+md puerts-node\lib\Win64\
+copy node\out\Release\libnode.dll .\puerts-node\lib\Win64\
+copy node\out\Release\libnode.exp .\puerts-node\lib\Win64\
+copy node\out\Release\libnode.lib .\puerts-node\lib\Win64\
