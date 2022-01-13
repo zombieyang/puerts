a489c5d8e253a9d9e4134cfc2ac6a97f28347303
zombieyang <348063288@qq.com>
2022-01-13 20:10:03 +0800

    eval bugfix

--- unity/native_src/Src/JSEngine_Eval.cpp
+++ unity/native_src/Src/JSEngine_Eval.cpp
@@ -5,6 +5,7 @@
 * This file is subject to the terms and conditions defined in file 'LICENSE', which is part of this source code package.
 */
 #include "JSEngine.h"
+#include "Log.h"
 #if WITH_QUICKJS
 #include "quickjs.h"
 #endif
@@ -110,16 +111,18 @@ namespace puerts {
         }
 
         size_t Length = 0;
-        char* Code;
-        bool IsCJSModule = !(name_length > 4 && name_std.substr(name_length - 4, name_length).compare(".mjs") == 0);
-        if (!IsCJSModule)
+        char* Code = JsEngine->ModuleResolver(name_std.c_str(), JsEngine->Idx, Length);
+        bool IsCJSModule = false;
+
+        if (Code == nullptr) 
         {
-            Code = JsEngine->ModuleResolver(name_std.c_str(), JsEngine->Idx, Length);
+            IsCJSModule = !(name_length > 4 && name_std.substr(name_length - 4, name_length).compare(".mjs") == 0);
+            if (!IsCJSModule) 
+            {
+                Code = (char *)(CjsModulePrepend + name_std + CjsModuleAppend).c_str();
+            }
         }
-        else 
-        {
-            Code = (char *)(CjsModulePrepend + name_std + CjsModuleAppend).c_str();
-        }
+        
         JSValue func_val;
         JSModuleDef* module_ = nullptr;
 
@@ -159,6 +162,8 @@ namespace puerts {
         }
 
         if (JS_IsException(func_val)) {
+            JSValue ex = JS_GetException(ctx);
+            auto msg = JS_ToCString(ctx, ex);
             return nullptr;
         }
 
@@ -269,27 +274,9 @@ namespace puerts {
             return false;
 
         } else {
-            // if (Exportee != nullptr) 
-            // {
-            //     JSModuleDef* EntryModule = (JSModuleDef *) JS_VALUE_GET_PTR(evalRet);
-                
-            //     val = MainIsolate->Alloc<v8::Value>();
-            //     val->value_ = js_get_module_ns(ctx, EntryModule);
-            //     JS_FreeValue(ctx, evalRet);
-            //     v8::Local<v8::Value> ns = v8::Local<v8::Value>(val);
-            //     if (Exportee == 0) 
-            //     {
-            //         ResultInfo.Result.Reset(Isolate, ns);
-            //     } 
-            //     else 
-            //     {
-            //         ResultInfo.Result.Reset(
-            //             Isolate, 
-            //             ns.As<v8::Object>()->Get(Context, FV8Utils::V8String(Isolate, Exportee)).ToLocalChecked()
-            //         );
-            //     }
-            // }
-
+            val = MainIsolate->Alloc<v8::Value>();
+            val->value_ = evalRet;
+            ResultInfo.Result.Reset(MainIsolate, v8::Local<v8::Value>(val));
             return true;
             
         }
