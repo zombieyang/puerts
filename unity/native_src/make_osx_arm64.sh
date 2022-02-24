78abe815fd797c8022d141337b700173300730e3
zombieyang <348063288@qq.com>
2022-02-24 11:34:49 +0800

    copied wrong lib

--- unity/native_src/make_osx_arm64.sh
+++ unity/native_src/make_osx_arm64.sh
@@ -12,7 +12,7 @@ cd ..
 cmake --build $OUTPUT --config Release
 cmake --install $OUTPUT --prefix "$(pwd)/$OUTPUT"
 mv $OUTPUT/bin/libpuerts.dylib $OUTPUT/bin/puerts.bundle
-cp -r $ENGINE/Lib/macOS/*.dylib $OUTPUT/bin/
+cp -r $ENGINE/Lib/macOS_arm64/*.dylib $OUTPUT/bin/
 
 mkdir -p ../Assets/Plugins/macOS_arm64
 cp -r $OUTPUT/bin/* ../Assets/Plugins/macOS_arm64/
