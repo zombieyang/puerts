2dca14e29a9f061d2f577f55afddf760ce357971
zombieyang(杨浩) <zombieyang@tencent.com>
2022-03-29 15:15:29 +0800

    dispose protect

--- unity/Assets/Puerts/Src/Resources/puerts/dispose.mjs
+++ unity/Assets/Puerts/Src/Resources/puerts/dispose.mjs
@@ -5,8 +5,9 @@ const goodbye = function() { throw new Error('puerts has disposed'); }
 
 puerts.loadType = goodbye
 puerts.getNestedTypes = goodbye
-
-setToGoodbyeFuncRecursive(require('csharp'));
+try {
+    setToGoodbyeFuncRecursive(require('csharp'));
+} catch(e) {}
 
 function setToGoodbyeFuncRecursive(obj) {
     Object.keys(obj).forEach(key=> {
@@ -18,12 +19,12 @@ function setToGoodbyeFuncRecursive(obj) {
         if (typeof obj[key] == 'function' && obj[key].prototype) {
             const prototype = obj[key].prototype;
             Object.keys(prototype).forEach((pkey)=> {
-                // if (Object.getOwnPropertyDescriptor(prototype, pkey).configurable) {
+                if (Object.getOwnPropertyDescriptor(prototype, pkey).configurable) {
                     Object.defineProperty(prototype, pkey, {
                         get: goodbye,
                         set: goodbye,
                     })
-                // }
+                }
             })
             Object.keys(obj[key]).forEach((skey)=> {
                 if (Object.getOwnPropertyDescriptor(obj[key], skey).configurable) {
