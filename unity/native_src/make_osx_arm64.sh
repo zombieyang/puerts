14ab45f41c3a11266a167eb047546a9dc478f3be
zombieyang <zombieyang@zombieyangdeMacBook-Pro.local>
2022-05-26 15:32:22 +0800

    [unity] change v8_9.6 to v8_9.4 & fix silicon blob error &  add vs2022 general project

--- unity/native_src/make_osx_arm64.sh
+++ unity/native_src/make_osx_arm64.sh
@@ -4,14 +4,19 @@ then
     ENGINE="v8"
 fi
 
+CONFIG=$2
+if [ "$2" == "" ]
+then
+    CONFIG="Release"
+fi
+
 OUTPUT=build_osx_arm64_$ENGINE
 
-mkdir -p $OUTPUT && cd $OUTPUT
+mkdir -p $OUTPUT/bin && cd $OUTPUT
 cmake -DJS_ENGINE=$ENGINE -DFOR_SILICON=ON -GXcode ../
 cd ..
-cmake --build $OUTPUT --config Release
-cmake --install $OUTPUT --prefix "$(pwd)/$OUTPUT"
-mv $OUTPUT/bin/libpuerts.dylib $OUTPUT/bin/puerts.bundle
+cmake --build $OUTPUT --config $CONFIG
+mv $OUTPUT/$CONFIG/libpuerts.dylib $OUTPUT/bin/
 cp -r $ENGINE/Lib/macOS_arm64/*.dylib $OUTPUT/bin/
 
 mkdir -p ../Assets/Plugins/arm64
